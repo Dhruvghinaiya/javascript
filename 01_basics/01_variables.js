9f05ab82a1d9f66d378ab810851759ba174f703b
Dhruv Ghinaiya <dhruvghinaiya793@gmail.com>
2026-01-04 13:00:41 +0530

    leaning varibles and types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1495
+let accountEmail = "dhruv@gmail.com"
+var  accountPassword = 123456
+accountCity = "vadodara"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
