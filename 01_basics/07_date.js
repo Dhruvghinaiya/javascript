5563b8944966740d2822be9b814f9f62a387fad5
Dhruv Ghinaiya <dhruvghinaiya793@gmail.com>
2026-01-05 13:55:46 +0530

    Learning date and function

---
+++ 01_basics/07_date.js
@@ -0,0 +1,18 @@
+let myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+
+let myCreateDate = new Date(2026,0,1);
+
+console.log(myCreateDate.toLocaleDateString());
+
+console.log(myDate.getMonth());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+
+
+
