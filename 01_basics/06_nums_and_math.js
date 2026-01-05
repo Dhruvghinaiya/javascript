515dd43802d071df0b976f36a8ade7b33697e401
Dhruv Ghinaiya <dhruvghinaiya793@gmail.com>
2026-01-05 13:29:43 +0530

    learning number and math function

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+let score = 125;
+
+console.log(typeof score.toString());
+
+let balance = 100;
+
+console.log(balance.toFixed(2));
+
+let otherNum = 123.8966;
+
+console.log(otherNum.toPrecision(4));
+
+let longNum = 10000000;
+console.log(longNum.toLocaleString());
+
+
+//+++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-14));
+console.log(Math.round(8.7));
+console.log(Math.floor(8.7));
+console.log(Math.ceil(8.2));
+console.log(Math.min(7,4,6,9));
+console.log(Math.max(7,4,6,9));
+
+console.log(Math.round((Math.random()*900000)+100000));
+
+
+
+
+
