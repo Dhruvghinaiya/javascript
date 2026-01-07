df80c4a7c543483824eb89cd124842cbf913e8d3
Dhruv Ghinaiya <dhruvghinaiya793@gmail.com>
2026-01-08 00:03:06 +0530

    learning while and do while loop

---
+++ 05_loop/two.js
@@ -0,0 +1,20 @@
+let a = 1;
+while (a <= 5) {
+  console.log(a);
+  a++;
+}
+
+let data = ["dhruv", "tushar", "ajay", "darshan"];
+
+let arr = 0;
+
+while (arr < data.length) {
+  console.log(data[arr]);
+  arr++;
+}
+
+let i = 8;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
